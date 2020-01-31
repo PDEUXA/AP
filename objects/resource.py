a3b79caa224b7e41bbb8ba90fdf69b1e6dae7068
jerevi <jer.vincent.hoa@gmail.com>
2020-01-31 13:09:55 +0100

    Initialisation

--- objects/resource.py
+++ objects/resource.py
@@ -3,11 +3,11 @@ class Resource:
     def __init__(self, resource):
         self.state = "Free"
         self.name = resource
-        self.current_task = 0
+        self.current_task = -1
         self.current_job = 0
 
     def __str__(self):
-        if self.current_task != 0:
+        if self.current_task != -1:
             return 'Machine number = {0}, State = {1}, Current Job = {2}, Current Task = {3}'.format(
                 self.name,
                 self.state,
